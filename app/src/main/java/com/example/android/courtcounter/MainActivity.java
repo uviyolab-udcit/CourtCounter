4a077f8786c39461e7003181652d9e3a60cb6268
Lloyd Napata <29262019+loynaps@users.noreply.github.com>
2018-06-01 20:48:32 +0200

    Add files via upload

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.courtcounter;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
@@ -41,15 +41,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given score for Team A.
-     */
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-
-    /**
      * Increase the score for Team B by 1 point.
      */
     public void addOneForTeamB(View v) {
@@ -74,12 +65,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v) {
+        int scoreTeamA = 0;
+        int scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
-
 
 }
